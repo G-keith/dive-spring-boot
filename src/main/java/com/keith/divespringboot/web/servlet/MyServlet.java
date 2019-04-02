099a3aa6600422cf63aa5ab48f9149068316b1fc
G-keith <1605763261@qq.com>
2019-04-02 15:18:17 +0800

    servlet案例

---
+++ src/main/java/com/keith/divespringboot/web/servlet/MyServlet.java
@@ -0,0 +1,21 @@
+package com.keith.divespringboot.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2019-04-02
+ */
+@WebServlet(urlPatterns = "/my/servlet")
+public class MyServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("HelloWorld");
+    }
+}
