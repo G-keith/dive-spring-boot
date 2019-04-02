fa9508d87d15f066be815815d0491606b2c923da
G-keith <1605763261@qq.com>
2019-04-02 14:29:15 +0800

    springboot核心学习

---
+++ src/test/java/com/keith/divespringboot/DiveSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.keith.divespringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiveSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
