b6121ad534c520efed0d8b6d6a5f5cea7418d616
G-keith <1605763261@qq.com>
2019-05-10 09:41:01 +0800

    自定义注解，compoment派生性

--- src/main/java/com/keith/divespringboot/configuare/bootstrap/RepositoryBootstrap.java
+++ src/main/java/com/keith/divespringboot/configuare/bootstrap/RepositoryBootstrap.java
@@ -1,7 +1,5 @@
 package com.keith.divespringboot.configuare.bootstrap;
 
-import com.keith.divespringboot.configuare.annotation.EnableHelloWorld;
-import com.keith.divespringboot.configuare.configuratrion.HelloWorldConfiguration;
 import com.keith.divespringboot.configuare.repository.MyFirstLevelRepository;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -13,18 +11,14 @@ import org.springframework.context.annotation.ComponentScan;
  * @version 1.0
  * @date 2019-05-07
  */
-//@ComponentScan("com.keith.divespringboot.configuare.repository")
-@EnableHelloWorld
+@ComponentScan("com.keith.divespringboot.configuare.repository")
 public class RepositoryBootstrap {
     public static void main(String[] args) {
         ConfigurableApplicationContext context=new SpringApplicationBuilder(RepositoryBootstrap.class).web(WebApplicationType.NONE).run(args);
 
         //检验bean是否存在
-//        MyFirstLevelRepository myFirstLevelRepository=context.getBean("myFirstLevelRepository", MyFirstLevelRepository.class);
-//        System.out.println(myFirstLevelRepository.toString());
-
-        String helloWorld=context.getBean("helloWorld",String.class);
-        System.out.println("Bean："+helloWorld);
+        MyFirstLevelRepository myFirstLevelRepository=context.getBean("myFirstLevelRepository", MyFirstLevelRepository.class);
+        System.out.println(myFirstLevelRepository.toString());
 
         context.close();
     }
